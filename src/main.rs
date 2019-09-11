035deafb1fcc70856c908f237b9397868da2e255
Simon Martin <smartin@scottlogic.com>
2019-09-11 07:03:30 +0000

    Wrap in infinite loop

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::net::{SocketAddr, TcpListener, TcpStream};
 use std::io;
-use std::thread;
 use std::io::{Read, Write};
+use std::{thread, time};
 
 fn server() -> io::Result<()> {
     let addrs = [
@@ -10,20 +10,21 @@ fn server() -> io::Result<()> {
     ];
     let listener = TcpListener::bind(&addrs[..])?;
     println!("listening on {:?}", listener.local_addr()?);
-    for stream in listener.incoming() {
-        thread::spawn(|| {
-        match stream {
-            Ok(mut stream) => {
-                println!("new client: {:?}", stream);
-                stream.write(b"Hello World\r\n").unwrap();
-                let mut buf = [128; 0];
-                stream.read(&mut buf).unwrap();
-                println!("{:?} read {:?}", stream, buf);
+    loop {
+        match listener.accept() {
+            Err(e) => println!("incoming error: {:?}", e),
+            Ok((mut stream, sockaddr)) => {
+                println!("new client: {:?}", sockaddr);
+                thread::spawn(move || {
+                    thread::sleep(time::Duration::from_secs(10));
+                    stream.write(b"Hello World\r\n").unwrap();
+                    let mut buf = [128; 0];
+                    stream.read(&mut buf).unwrap();
+                    println!("{:?} read {:?}", stream, buf);
+                });
             }
-            Err(e) => println!("incoming error: {:?}", e)
-        }
-        });
-    };
+        };
+    }
     Ok(())
 }
 
